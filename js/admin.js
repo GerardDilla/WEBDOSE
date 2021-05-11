50de838d0640bb07c0298777f4aa6a72b5c3bf0a
jfabregassdca <jfabregas@sdca.edu.ph>
2021-05-11 16:37:54 +0800

    feat: submit when click enter (Login)

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,9 @@
+$(document).on('keyup',function(e){
+    // console.log(e.keyCode)
+    if(e.keyCode==13){
+        $('#sign_in').submit();
+    }
+})
 if (typeof jQuery === "undefined") {
     throw new Error("jQuery plugins need to be before this file");
 }
