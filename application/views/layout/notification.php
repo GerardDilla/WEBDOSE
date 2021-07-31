af486dc5d5265c680db92e7210acdb5b1d3eea02
Jhon Norman Fabregas <jfabregas@sdca.edu.ph>
2021-07-31 21:13:42 +0800

    fix conflicts

--- application/views/layout/notification.php
+++ application/views/layout/notification.php
@@ -1,8 +1,8 @@
 <script src="https://unpkg.com/@feathersjs/client@^4.3.0/dist/feathers.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js"></script>
 <script>
-    const socket = io('https://stdominiccollege.edu.ph:4003'); 
-    // const socket = io('http://10.0.0.81:4004');
+    // const socket = io('https://stdominiccollege.edu.ph:4003'); 
+    const socket = io('http://10.0.0.81:4004');
 
     const app = feathers();
     async function getNotifications(){
@@ -11,9 +11,13 @@
         $('#notification-div').empty();
         var chat_html = '';
         chat_notification.forEach((item)=>{
+            var text = item.last_message;
+            var count = 50;
+            var result = text.slice(0, count) + (text.length > count ? "..." : "");
+
             chat_html += `
             <a href="#" class="notification-info">
-                <div class="notification-student-name">${item.full_name}</div><div class="notification-student-message">${item.message}</div><span class="notification-time">5 hrs ago</span>
+                <div class="notification-student-name">${item.full_name}</div><div class="notification-student-message">${result}</div><span class="notification-time">5 hrs ago</span>
             </a>
             `;
         })
