2cf8e1b1705a7d5bfcf510e124e4f968b2d59a24
Jhon Norman Fabregas <jfabregas@sdca.edu.ph>
2021-08-05 10:44:58 +0800

    fixes for schedule checker on scheduling

--- application/config/database.php
+++ application/config/database.php
@@ -76,36 +76,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => '10.0.0.9',
-// 	'username' => 'schoolsysdb',
-// 	'password' => 'Wpd2$Ya=$dCA<KXM25>',
-// 	// 'database' => 'Schoolsysdb_dev',
-// 	'database' => 'schoolsysdb',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => TRUE,
-// 	'db_debug' => TRUE,
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'asdf',
+	'hostname' => '10.0.0.9',
+	'username' => 'schoolsysdb',
+	'password' => 'Wpd2$Ya=$dCA<KXM25>',
+	// 'database' => 'Schoolsysdb_dev',
 	'database' => 'schoolsysdb',
 	'dbdriver' => 'mysqli',
-	'dbprefix' => '', 
+	'dbprefix' => '',
 	'pconnect' => TRUE,
 	'db_debug' => TRUE,
 	'cache_on' => FALSE,
@@ -118,4 +97,25 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-); 
+);
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => 'asdf',
+// 	'database' => 'schoolsysdb',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '', 
+// 	'pconnect' => TRUE,
+// 	'db_debug' => TRUE,
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// ); 
--- application/views/layout/notification.php
+++ application/views/layout/notification.php
@@ -3,20 +3,23 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/timeago.js/2.0.2/timeago.min.js" integrity="sha512-sl01o/gVwybF1FNzqO4NDRDNPJDupfN0o2+tMm4K2/nr35FjGlxlvXZ6kK6faa9zhXbnfLIXioHnExuwJdlTMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
-    // const socket = io('https://stdominiccollege.edu.ph:4003');
+    const socket = io('https://stdominiccollege.edu.ph:4003');
     // const socket = io('http://10.0.0.81:4004');
-    const socket = io('http://localhost:4004');
+    
+    // const socket = io('http://localhost:4004');
     const app = feathers();
     async function getNotifications(){
+        
         const chat_notification = await app.service('chat-notification').find();
         console.log(chat_notification)
+        $('.notification-number').css('display','none')
         if(chat_notification.notif_number>0){
             $('.notification-number').text(chat_notification.notif_number);
             $('.notification-number').css('display','inherit')
         }
-        else{
-            $('.notification-number').css('display','none')
-        }
+        // else{
+        //     $('.notification-number').css('display','none')
+        // }
         $('#notification-div').empty();
         var chat_html = '';
         chat_notification.notif.forEach((item)=>{
